89254599662ee6db1d2b840be4b428ec37f998cd
Songseulhwa <88119718+Songseulhwa@users.noreply.github.com>
2022-07-15 19:23:45 +0900

    Delete config.json
    토큰이 있어 삭제합니다.

--- deploy-commands.js
+++ deploy-commands.js
@@ -13,6 +13,21 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+// rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+// 	.then(() => console.log('Successfully registered application commands.'))
+// 	.catch(console.error);
+
+
+(async () => {
+	guildIds.map(async (guildId) => {
+		try {
+			await rest.put(
+				Routes.applicationGuildCommands(clientId, guildId),
+				{ body: {} },
+			);
+			console.log(`${guildId}서버 등록 성공`);
+		} catch (error) {
+			console.error(error);
+		}
+	});
+})
